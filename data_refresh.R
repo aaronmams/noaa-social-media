2014d0aadde25609477e13d28830b1da62b9f9f7
Aaron Mamula <1380587960@mil>
2018-02-20 13:31:53 -0800

    redo data import script

--- data_refresh.R
+++ data_refresh.R
@@ -2,50 +2,116 @@ require(twitteR)
 library(data.table)
 library(dplyr)
 library(ggplot2)
+library(rtweet)
+library(RODBC)
 
-#--------------------------------------------------------------------------
-#first read in the current .csv file
+#------------------------------------------------------------------
+#Twitter credentials
 
-tweets.ref <- read.csv('data/tweets.csv')
-user.ref <- read.csv('data/user_info.csv')
-#---------------------------------------------------------------------------
+#pw: Nmfsswfsc2016#
+#username: aaronmams
+#email: aaron.mams@gmail.com
+#app name: mamultron
+#owner: mamulsaurus
+#owner id: 750554190489985025
 
-#----------------------------------------------------------------------------
-#Now we do a data pull 
 
-users <- c('@NOAAHabitat','@NOAAFisheries','@NOAAFish_WCRO','@NOAA','@NOAAResearch')
+# consumer key:
+# c71wImbS4aZnwA0yKx14Z5cxz
+
+# consumer secret
+#HfRuYxKvAoXuTwTwUa9vslKlsB3AL5kQLv40U12Xsxz1ybkhuX
+
+# access token
+#750554190489985025-aOkEUbRXEXeyaKpHmM2VX5ZJ7WhoM7j
+
+#access token secret
+# 66o8C4tzc8LaqZNCE1XoJDU4QLFyPXu2qlUk3JlHBxd71
+
+# this should be uncessary now
+#consumer_key <- 'c71wImbS4aZnwA0yKx14Z5cxz'
+#consumer_secret <- 'HfRuYxKvAoXuTwTwUa9vslKlsB3AL5kQLv40U12Xsxz1ybkhuX'
+#access_token <- '750554190489985025-aOkEUbRXEXeyaKpHmM2VX5ZJ7WhoM7j'
+#access_secret <- '66o8C4tzc8LaqZNCE1XoJDU4QLFyPXu2qlUk3JlHBxd71'
+
+#twitter_token <- create_token(app='mamultron',
+#                              consumer_key=consumer_key,
+#                              consumer_secret=consumer_secret)
+
+#setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+#------------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------
+# get the old stuff
+channel <- odbcConnect(dsn='pinniger', uid='aaron.mamula',pwd='aaron24')
+content <- sqlQuery(channel,'select * from [PacFIN_Econ].[dbo].[tweets_content]')
+users.info <- sqlQuery(channel,'select * from [PacFIN_Econ].[dbo].[tweets_users]')
+close(channel)
+#-------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------
+#pull some new stuff
+
+users <- c('@NOAAHabitat','@NOAAFisheries','@NOAAFish_WCRO','@NOAA','@NOAAResearch','@NOAANCEIclimate',
+           '@NOAAClimate','@MBNMS','@NOAA_CINMS','@Eileen_NOAAFish','@NOAAFisheriesAK','@NOAAFish_PIRO',
+           '@NOAAFish_PIFSC','@NOAAFish_SERO','@CenterForBioDiv')
 
 # get tweets from each of our accounts
-tl <- get_timeline('@NOAAHabitat',n=1000)
-tl <- rbind(tl,get_timeline('@NOAAFisheries',n=1000))
-tl <- rbind(tl,get_timeline('@NOAAFish_WCRO',n=1000))
-tl <- rbind(tl,get_timeline('@NOAA',n=500))
-tl <- rbind(tl,get_timeline('@NOAAResearch',n=1000))
+tl <- get_timeline('@NOAAHabitat',n=200,retryonratelimit=TRUE)
+tl <- rbind(tl,get_timeline('@NOAAFisheries',n=200))
+tl <- rbind(tl,get_timeline('@NOAAFish_WCRO',n=200))
+tl <- rbind(tl,get_timeline('@NOAA',n=200))
+tl <- rbind(tl,get_timeline('@NOAAResearch',n=200))
+
+tl <- rbind(tl,get_timeline('@NOAANCEIclimate',n=1000))
+tl <- rbind(tl,get_timeline('@NOAAClimate',n=1000))
+tl <- rbind(tl,get_timeline('@MBNMS',n=1000))
+tl <- rbind(tl,get_timeline('@Eileen_NOAAFish',n=1000))
+tl <- rbind(tl,get_timeline('@NOAAFisheriesAK',n=1000))
+tl <- rbind(tl,get_timeline('@NOAAFish_PIRO',n=1000))
+tl <- rbind(tl,get_timeline('@NOAAFish_PIFSC',n=1000))
+tl <- rbind(tl,get_timeline('@NOAAFish_SERO',n=1000))
+tl <- rbind(tl,get_timeline('@CenterforBioDiv',n=1000))
+
 
 tl <- tl %>% select(created_at,user_id,screen_name,text,
                     is_quote,is_retweet,favorite_count,
                     retweet_count,hashtags,mentions_screen_name) %>%
-       mutate(update_time=Sys.time())
+  mutate(update_time=Sys.time())
 
 #fix hashtags
-hash <- unlist(lapply(tl$hashtags,function(x){collapse(c(unlist(x)),sep=";")}))
+hash <- unlist(lapply(tl$hashtags,function(x){paste(c(unlist(x)),collapse=";")}))
+
 tl <- tl %>% mutate(hash1=hash) %>%
   select(-hashtags) %>%
   mutate(hashtags=hash1) %>%
   select(-hash1)
 
 #fix @ mentions
-mentions <- unlist(lapply(tl$mentions_screen_name,function(x){collapse(c(unlist(x)),sep=";")}))
+mentions <- unlist(lapply(tl$mentions_screen_name,function(x){paste(c(unlist(x)),collapse=";")}))
 tl <- tl %>% mutate(mentions1=mentions) %>%
   select(-mentions_screen_name) %>%
   mutate(mentions=mentions1) %>%
   select(-mentions1)
 
+
+#-----------------------------------------------------------------------
+
+
 #-----------------------------------------------------------------------------------
 #get tweets mentioning user, save them as a data frame, then bind the data frames
-df <- lapply(users,function(x){
+
+dummy.df <- search_tweets('#love',n=10)
+
+df <- lapply(users[1:5],function(x){
   rt <- search_tweets(
-    x, n = 1000, include_rts = FALSE
+    x, n = 500, include_rts = FALSE
   )
   df <- rt %>% select(created_at,user_id,screen_name,text,
                       is_quote,is_retweet,favorite_count,
@@ -54,48 +120,125 @@ df <- lapply(users,function(x){
   return(df)  
 })
 
-df <- data.frame(rbindlist(df))
+
+df2 <- lapply(users[6:10],function(x){
+  rt <- search_tweets(
+    x, n = 500, include_rts = TRUE
+  )
+  
+  if(nrow(rt)==0){
+    df <- dummy.df %>%  select(created_at,user_id,screen_name,text,
+                               is_quote,is_retweet,favorite_count,
+                               retweet_count,hashtags,mentions_screen_name) %>% 
+      mutate(update_time=Sys.time()) 
+  }else{
+    df <- rt %>% select(created_at,user_id,screen_name,text,
+                        is_quote,is_retweet,favorite_count,
+                        retweet_count,hashtags,mentions_screen_name) %>% 
+      mutate(update_time=Sys.time())
+  }
+  return(df)  
+})
+
+df3 <- lapply(users[11:15],function(x){
+  rt <- search_tweets(
+    x, n = 500, include_rts = TRUE
+  )
+  
+  if(nrow(rt)==0){
+    df <- dummy.df %>%  select(created_at,user_id,screen_name,text,
+                               is_quote,is_retweet,favorite_count,
+                               retweet_count,hashtags,mentions_screen_name) %>% 
+      mutate(update_time=Sys.time()) 
+  }else{
+    df <- rt %>% select(created_at,user_id,screen_name,text,
+                        is_quote,is_retweet,favorite_count,
+                        retweet_count,hashtags,mentions_screen_name) %>% 
+      mutate(update_time=Sys.time())
+  }
+  return(df)  
+})
+
+df <- data.frame(rbind(rbindlist(df),rbindlist(df2),rbindlist(df3)))
 
 #fix hashtag list
-hash <- unlist(lapply(df$hashtags,function(x){collapse(c(unlist(x)),sep=";")}))
+hash <- unlist(lapply(df$hashtags,function(x){paste(c(unlist(x)),collapse=";")}))
 df <- df %>% mutate(hash1=hash) %>%
   select(-hashtags) %>%
   mutate(hashtags=hash1) %>%
   select(-hash1)
 
 #fix the @ mention list
-mentions <- unlist(lapply(df$mentions_screen_name,function(x){collapse(c(unlist(x)),sep=";")}))
+mentions <- unlist(lapply(df$mentions_screen_name,function(x){paste(c(unlist(x)),collapse=";")}))
 df <- df %>% mutate(mentions1=mentions) %>%
   select(-mentions_screen_name) %>%
   mutate(mentions=mentions1) %>%
   select(-mentions1)
 #--------------------------------------------------------------------------
 
+#--------------------------------------------------------------------------
+#get the user info
 
-user.info <- lapply(users,function(x){
+new.user.info <- lapply(users,function(x){
   user.info <- search_users(x) %>% 
     select(user_id,name,screen_name,location,description,followers_count,
            friends_count)
   return(user.info)
 })
 
-user.info <- data.frame(rbindlist(user.info))
-#---------------------------------------------------------------------------------
+new.user.info <- data.frame(rbindlist(new.user.info))
 
-#--------------------------------------------------------------------------------
-#add any new tweets to the data set
-tweets <- rbind(df,tl)
-tweets <- rbind(tweets.ref,df) %>% group_by(user_id,created_at,text) %>% 
-             filter(row_number() ==1)
+new.user.info <- new.user.info %>% mutate(updated=as.POSIXct(Sys.time()))
+#--------------------------------------------------------------------------
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#Now we have updated information and we want to:
 
-write.csv(tweets,file='data/tweets.csv')
-#--------------------------------------------------------------------------------
+# 1. append the new information to the old info in a data frame
+# 2. push that data frame up to the PacFIN_Econ database
+
+content <- rbind(content,tl,df)
+users.info <- rbind(users.info,new.user.info) 
+
+#save these as back up .csv file
+write.csv(content,'data/tweet_content.csv')
+write.csv(user.info,'data/tweet_users.csv')
+
+#Final Step: push these data frames to the databases
+
+# First, the content data frame
+
+# the database tables
+
+channel <- odbcConnect(dsn='pinniger', uid='aaron.mamula',pwd='aaron24')
+sqlQuery(channel,'USE PacFIN_Econ')
+cols <- sqlColumns(channel,"tweets_content")
+colTypes <- as.character(cols$TYPE_NAME)
+colTypes[which(colTypes=='bit')] <- 'varchar(5)'
+colTypes[which(colTypes=='varchar')] <- 'varchar(255)'
+names(colTypes) <- as.character(cols$COLUMN_NAME)
+sqlQuery(channel,"drop table tweets_content")
+sqlSave(channel,content,tablename='tweets_content',append=T,rownames=F,varTypes=colTypes)
+close(channel)
 
 
-#---------------------------------------------------------------------------------
-# for the user info we just bind it to what was already there
+channel <- odbcConnect(dsn='pinniger', uid='aaron.mamula',pwd='aaron24')
+sqlQuery(channel,'USE PacFIN_Econ')
+cols.users <- sqlColumns(channel,'tweets_users')
+colTypes <- as.character(cols.users$TYPE_NAME)
+colTypes[which(colTypes=='bit')] <- 'varchar(5)'
+colTypes[which(colTypes=='varchar')] <- 'varchar(500)'
+names(colTypes) <- as.character(cols.users$COLUMN_NAME)
+sqlQuery(channel,"drop table tweets_users")
+sqlSave(channel,users.info,tablename='tweets_users',append=T,rownames=F,varTypes=colTypes)
+close(channel)
 
-user.info <- rbind(user.ref,user.info)
-write.csv(user.info,'data/user_info.csv')
+#--------------------------------------------------------------------------
 
-#---------------------------------------------------------------------------------
--- engagement_indicators.R
+++ engagement_indicators.R
@@ -1,5 +1,10 @@
 rm(list=ls())
 library(gridExtra)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(lubridate)
+library(ggthemes)
 
 tweets <- read.csv('data/tweets.csv')
 user.info <- read.csv('data/user_info.csv')
@@ -10,33 +15,182 @@ NOAA_screen_names <- c('NOAAHabitat','NOAAFisheries','NOAAFish_WCRO','NOAA','NOA
 #----------------------------------------------------------
 # most retweeted hashtags from NOAA accounts
 noaa.tweets <- tbl_df(tweets) %>% filter(screen_name %in% NOAA_screen_names) %>%
-                 group_by(screen_name,created_at,text) %>%
-                 filter(row_number()==1)
+  group_by(screen_name,created_at,text) %>%
+  filter(row_number()==1)
 
 hash.list <- list()
 for(i in 1:nrow(noaa.tweets)){
   h <- strsplit(as.character(noaa.tweets$hashtags[i]),";")
   rt <- noaa.tweets$retweet_count[i]
   user <- noaa.tweets$screen_name[i]
-hash.list[[i]] <- data.frame(rt=rt,hashtag=unlist(h),user=user)  
+  hash.list[[i]] <- data.frame(rt=rt,hashtag=unlist(h),user=user)  
 }  
-  
+
 hashtags <- data.frame(rbindlist(hash.list))
 hashtags$hashtag <- tolower(hashtags$hashtag)
 
 hashtags <- tbl_df(hashtags) %>% group_by(user,hashtag) %>% 
-              summarise(retweets=sum(rt)) %>% arrange(-retweets)
+  summarise(retweets=sum(rt)) %>% arrange(-retweets)
+
+
 
 #plot top 10 most retweeted hashtags for each account
 plot.data <- hashtags %>% group_by(user) %>% arrange(-retweets) %>%
-               filter(row_number()<=10) %>% 
-              filter(user=='NOAAResearch') %>%
-              arrange(retweets) %>%
-              mutate(hashtag=factor(hashtag,hashtag)) 
+  filter(row_number()<=10) %>% 
+  arrange(user,retweets) %>%
+  mutate(hashtag=factor(hashtag,hashtag)) 
 
-ggplot(plot.data,
+ggplot(subset(plot.data,user %in% c('NOAAFish_WCRO','NOAAFisheries','NOAAHabitat')),
+       aes(x=retweets,y=hashtag,xmin = 0, xmax = retweets)) + geom_point()  + 
+  geom_errorbarh(height = .1) + 
+  facet_grid(user~.,scales='free',space='free') + theme_bw()
+
+
+#----------------------------------------------------------
+
+#----------------------------------------------------------
+# most retweeted hashtags from @ mentions
+
+tweets.non.noaa <- tweets %>% filter(!screen_name %in% NOAA_screen_names)
+
+hash.list <- list()
+for(i in 1:nrow(tweets.non.noaa)){
+  h <- strsplit(as.character(tweets.non.noaa$hashtags[i]),";")
+  rt <- tweets.non.noaa$retweet_count[i]
+  user <- tweets.non.noaa$screen_name[i]
+  hash.list[[i]] <- data.frame(rt=rt,hashtag=unlist(h),user=user)  
+}  
+
+hashtags <- data.frame(rbindlist(hash.list))
+hashtags$hashtag <- tolower(hashtags$hashtag)
+
+hashtags <- tbl_df(hashtags) %>% group_by(user,hashtag) %>% 
+  summarise(retweets=sum(rt)) %>% arrange(-retweets)
+
+ggplot(hashtags,
        aes(x=hashtag,y=retweets)) + geom_bar(stat='identity') + coord_flip() + 
-       theme_bw() + ggtitle('@NOAAResearch')
+  theme_bw() + ggtitle('@NOAAResearch')
+
+#----------------------------------------------------------
+
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+
+#----------------------------------------------------------
+# Engagement overtime
+
+#choose a time window
+start <- as.Date('2018-01-01')
+end <- as.Date('2018-02-11')
+
+noaa.tweets <- tbl_df(tweets) %>% filter(screen_name %in% NOAA_screen_names) %>%
+  filter(as.Date(created_at,format="%Y-%m-%d %H:%M:%S") <= end 
+         & as.Date(created_at,format="%Y-%m-%d %H:%M:%S")>= start) %>%
+  group_by(screen_name,created_at,text) %>%
+  filter(row_number()==1)
+
+
+hash.list <- list()
+for(i in 1:nrow(noaa.tweets)){
+  h <- strsplit(as.character(noaa.tweets$hashtags[i]),";")
+  rt <- noaa.tweets$retweet_count[i]
+  user <- noaa.tweets$screen_name[i]
+  created <- noaa.tweets$created_at[i]
+  hash.list[[i]] <- data.frame(rt=rt,hashtag=unlist(h),user=user,created=created)  
+}  
+
+hashtags <- data.frame(rbindlist(hash.list))
+hashtags$hashtag <- tolower(hashtags$hashtag)
+hashtags <- tbl_df(hashtags) %>% 
+  group_by(hashtag) %>%
+  mutate(created=as.Date(created,format='%Y-%m-%d %H:%M:%S')) %>%
+  mutate(total=sum(rt)) %>%
+  ungroup() %>%
+  arrange(-total) %>%
+  mutate(rank=dense_rank(-total))  %>%
+  mutate(hashtag=ifelse(is.na(hashtag),"",hashtag))
+
+#1 row per hashtag used in the time window
+tags <- hashtags %>% group_by(hashtag) %>%
+  filter(row_number()==1) %>% 
+  mutate(start=start,end=end)
+
+
+tags <- tags %>%
+  rowwise() %>%
+  do(data.frame(hashtag=.$hashtag, created=seq(.$start,.$end,by="1 day"))) %>%
+  mutate(rtweets=0) 
+
+hashtags <- hashtags %>% group_by(hashtag,created) %>% 
+  summarise(rt=sum(rt))
+
+df <- tags %>% 
+  left_join(hashtags,by=c('hashtag','created')) %>%
+  mutate(rt=ifelse(is.na(rt),rtweets,rt)) %>%
+  mutate(rts=max(rt,rtweets)) 
+
+df <- tbl_df(df) %>%  
+  group_by(hashtag) %>% arrange(hashtag,created) %>%
+  mutate(retweets=cumsum(rts)) 
+
+
+#plot the top 20 hashtags during the time window
+ht.total <- df %>% group_by(hashtag) %>% summarise(total=max(retweets,na.rm=T)) %>%  
+  arrange(-total) %>% ungroup() %>% filter(row_number() <=20) %>%
+  mutate(hashtag=factor(hashtag))
+
+ggplot(ht.total,aes(x=reorder(hashtag,total),y=total)) + geom_bar(stat='identity',color='blue',alpha=0.6) + coord_flip() + 
+  theme_wsj() + ggtitle('Total Hashtag Retweets')
+
+#plot the top 5 non-empty hashtags by day over the time window
+df <- df %>% filter(hashtag!="") %>%
+  mutate(total=max(retweets,na.rm=T)) %>% 
+  ungroup() %>% 
+  mutate(rank=dense_rank(-total))  %>%
+  filter(rank<=5) %>%
+  arrange(rank) 
+
+ggplot(df,aes(x=created,y=retweets,color=hashtag,shape=hashtag)) + geom_line() + geom_point() + 
+  theme_wsj() + ggtitle('Retweets')
+
+
+#----------------------------------------------------------------------------------------
+
+
+
+#1 row per hashtag used in the time window
+tags <- hashtags %>% group_by(hashtag) %>%
+  filter(row_number()==1) %>% 
+  mutate(start=start,end=end)
+
+
+tags <- tags %>%
+  rowwise() %>%
+  do(data.frame(hashtag=.$hashtag, created=seq(.$start,.$end,by="1 day"))) %>%
+  mutate(rtweets=0) 
+
+hashtags <- hashtags %>% group_by(hashtag,created) %>% 
+  summarise(rt=sum(rt))
+
+df <- tags %>% 
+  left_join(hashtags,by=c('hashtag','created')) %>%
+  mutate(rt=ifelse(is.na(rt),rtweets,rt)) %>%
+  mutate(rts=max(rt,rtweets)) 
+
+df <- tbl_df(df) %>%  
+  group_by(hashtag) %>% arrange(hashtag,created) %>%
+  mutate(retweets=cumsum(rts)) 
+
+
+ggplot(df,
+       aes(x=created,y=retweets,color=hashtag)) + geom_line() +
+  theme_bw()
+
 
 
 #----------------------------------------------------------
